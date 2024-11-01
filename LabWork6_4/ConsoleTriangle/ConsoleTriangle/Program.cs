b78907cb659db94b31ba388329042c5610061f8f
Sergey Kiselyov <kiselyovsv@ya.ru>
2024-11-02 01:38:22 +0300

    The and of the 6th LabWork

---
+++ LabWork6_4/ConsoleTriangle/ConsoleTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ConsoleTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle A = new Triangle(5, 10, 20);
+            Print(A);
+            Triangle F = new Triangle(15, 10, 20);
+            Print(F);
+            Triangle G = new Triangle(8, 8, 8);
+            Print(G);
+            Triangle J = new Triangle(5, -18, 20);
+            Print(J);
+        }
+        public static void Print(Triangle t)
+        {
+            Console.WriteLine(t.OllSides);
+            if (Triangle.Perimeter(t) == 0 || Triangle.Area(t) == 0) Console.WriteLine("Треугольник не существует!");
+            else
+            {
+                Console.WriteLine($"Периметр треугольника равен:{Triangle.Perimeter(t)}.");
+                Console.WriteLine($"Площадь треугольника равна:{Triangle.Area(t):F2}.");
+            }
+        }
+    }
+
+}
---
+++ LabWork6_4/ConsoleTriangle/ConsoleTriangle/Triangle.cs
@@ -0,0 +1,66 @@
+﻿
+internal class Triangle
+{
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    public double SideA
+    {
+        get => sideA;
+
+        set
+        {
+            if (value > 0) sideA = value;
+            else Console.WriteLine("\nЗначение должно быть больше 0.");
+        }
+    }
+    public double SideB
+    {
+        get => sideB;
+
+        set
+        {
+            if (value > 0) sideB = value;
+            else Console.WriteLine("\nЗначение должно быть больше 0.");
+        }
+    }
+    public double SideC
+    {
+        get => sideC;
+
+        set
+        {
+            if (value > 0) sideC = value;
+            else Console.WriteLine("\nЗначение должно быть больше 0.");
+        }
+    }
+    public string OllSides
+    {
+        get => $"Сторона А равна:{sideA}, сторона В равна:{sideB}, сторона С равна:{sideC}"; 
+    }
+
+    public Triangle(double sideA, double sideB, double sideC)
+    {
+        SideA = sideA;
+        SideB = sideB;
+        SideC = sideC;
+    }
+
+    internal static double Perimeter(Triangle t)
+    {
+        if (Area(t) > 0)
+        {
+            double sum = t.sideA + t.sideB + t.sideC;
+            return sum;
+        }
+        else return 0;
+    }
+
+    internal static double Area(Triangle t)
+    {
+        double half = (t.sideA+t.sideB+t.sideC)/2;
+        double area = Math.Sqrt(half*(half-t.sideA)*(half-t.sideB)*(half-t.sideC));
+        if (area is double.NaN) return 0;
+        else return area;
+    }
+}
